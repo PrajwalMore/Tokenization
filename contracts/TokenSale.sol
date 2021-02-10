46066d5439e255f1e22f6cab73d975b38fe3f0a7
CodeStorm <31198108+PrajwalMore@users.noreply.github.com>
2021-02-10 12:53:22 +0530

    Update TokenSale.sol

--- contracts/TokenSale.sol
+++ contracts/TokenSale.sol
@@ -2,8 +2,6 @@ pragma solidity ^0.6.0;
 import "./Crowdsale.sol";
 import "./KycContract.sol";
 
-//import "D:/Blockchain-Development/1.Projects/1.Tokenization/contracts/KycContract.sol";
-
 contract TokenSale is Crowdsale{
     KycContract kyc;
     constructor(
@@ -17,18 +15,11 @@ contract TokenSale is Crowdsale{
         kyc=_kyc;
     }
     
-    function _preValidatePurchase(address beneficiary, uint256 weiAmount)  public payable override//internal view
+    function _preValidatePurchase(address beneficiary, uint256 weiAmount)  public payable override
     //Base functions can be overridden by inheriting contracts to change their behavior if they are marked as virtual .
     {
         super._preValidatePurchase(beneficiary,weiAmount);
         
     }
-    
-    function returnKycStatus() public view /*returns(bool)*/{
-        // bool kycStatus;
-        // kycStatus=kyc.kycCompleted(msg.sender);
-        require(kyc.kycCompleted(msg.sender),"KYC not completed yet,aborting...");
-        // return kycStatus;
-     }
     
 }
